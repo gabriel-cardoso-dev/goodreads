4b1581458b19dae971dbafc8d3153fa1864b8cfb
Gabriel Cardoso <55664632+gabriel-cardoso-dev@users.noreply.github.com>
2022-08-02 21:08:50 +0100

    setting up the connection with the cassandra instance in datastax

---
+++ src/test/java/dev/gabrielcardoso/goodreadsdataloader/GoodreadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielcardoso.goodreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodreadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
